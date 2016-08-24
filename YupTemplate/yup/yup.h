5323e9f8dfc971bead29417f7d110768666be642
Yu-hsien Chang <yhnchang@gmail.com>
2016-08-24 16:37:34 -0700

    Updated pathtool

--- YupTemplate/yup/yup.h
+++ YupTemplate/yup/yup.h
@@ -20,4 +20,7 @@
 #define BEGIN_NAMESPACE_YUP_GL namespace yup { namespace gl {
 #define END_NAMESPACE_YUP_GL } }
 
+#define BEGIN_NAMESPACE_YUP_PATH namespace yup { namespace path {
+#define END_NAMESPACE_YUP_PATH } }
+
 #define SAFE_DELETE(p) if(p) { delete p; p = nullptr; }
