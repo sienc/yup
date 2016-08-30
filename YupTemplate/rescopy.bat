6dbd1aadae46cd5db1d1bc815ebc5488e5d27904
Yu-hsien Chang <yhnchang@gmail.com>
2016-08-30 09:24:52 -0700

    Updated post build copy method

--- YupTemplate/rescopy.bat
+++ YupTemplate/rescopy.bat
@@ -1,9 +1,23 @@
 @echo off
 
+::Define newline variable
+(SET LF=^
+%=this line is empty=%
+)
+
 IF NOT "%RES_FILES%" == "" (
 echo Copying DLLs and resources to output folder...
-FOR %%I IN (%RES_FILES%) DO (
+
+:: Save file list to a temporary file, replacing ; with newline
+echo %RES_FILES:;=!LF!% > rescopy.tmp
+
+:: Loop through items in the file
+FOR /F %%I IN (rescopy.tmp) DO (
+echo Copying %%I
 xcopy %%I "%OUT_DIR%" /D /Y /S
 )
+
+:: delete temp file
+del rescopy.tmp
 
 )
